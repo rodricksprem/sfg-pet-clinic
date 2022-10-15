ad42a2a361601452564515b62866c43ee367c40b
rodricks <rodricks.premkumar@biogen.com>
2022-10-15 21:41:25 +0530

    Menu Links are Broken. closes #34

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +9,11 @@ public class IndexController {
     @RequestMapping({"","/","index","index.html"})
     public String index(){
         return "index";
+    }
+    @RequestMapping({"/oups"})
+    public String oupshandler(){
+
+        return "notimplemented";
+
     }
 }
