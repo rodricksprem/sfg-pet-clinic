7e80d9e4b9a43302caa8bd367fcef742eed9feb6
rodricks <rodricks.premkumar@biogen.com>
2022-10-15 18:24:00 +0530

    Added Visits, updated Owner with Pet relationship, closes #16

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity{
+    private LocalDate date;
+    private String description;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
