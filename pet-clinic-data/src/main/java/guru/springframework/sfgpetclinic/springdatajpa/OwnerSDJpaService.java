4487d5af276a42ccd37159274200c65df42fff7f
rodricks <rodricks.premkumar@biogen.com>
2022-10-17 10:27:45 +0530

    Created Spring Data JPA for Specialty Service. closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/springdatajpa/OwnerSDJpaService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
 @Service
-@Profile("OwnerSDJaService")
+@Profile("OwnerSDJpaService")
 public class OwnerSDJpaService implements OwnerService {
     private final OwnerRepository ownerRepository;
 
